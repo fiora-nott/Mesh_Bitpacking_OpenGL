77be84b37527dff27b5c09e71ec8cf840b98404b
Fiora Nott <155269673+fiora-nott@users.noreply.github.com>
2023-12-30 22:18:39 -0600

    Update ChunkMesher.cpp

--- OpenGL_Voxels_CDV1/src/ChunkMesher.cpp
+++ OpenGL_Voxels_CDV1/src/ChunkMesher.cpp
@@ -143,49 +143,64 @@ namespace ChunkMesher {
 					Block blockType = chunk->Get_Block(blockPos);
 					if (blockType == Block::AIR)
 						continue;
-
+					
+					// Iterate over each face of this voxel
 					for (int side = 0; side < 6; side++) {
+						// Project out with a normal vector and check if the neighbor voxel is solid or air
 						checkPos = blockPos + sideNormals[side];
 
+						// If the neighbor is opaque/non-air, do not draw a face. This face is occluded.
 						if (GetBlockFreeform(mem, checkPos, chunk) != Block::AIR)
 							continue;
 
-						//all conditions are met so draw the side
+						// All conditions are met so draw the side
 						float* localVerts = sideVertices[side];
 
-						//i represents vertex number
+						// i represents vertex number
 						for (int i = 0; i < 4; i++) {
+							// Grab the vertex displacement based on which vertex we are assigning
 							int3 vertLocalPos = int3(localVerts[i * 3], localVerts[i * 3 + 1], localVerts[i * 3 + 2]);
 							int3 vertObjectPos = blockPos + vertLocalPos;
-							//int3 vertWorldPos = vertLocalPos + blockPos + worldPos;
 
-							//Ambient occlusion actually has a bias since quads are triangles, and will work differently
-							//at different diagonals for every plane. This MAY BE WORTH FIXING!!!!!!!!!!!!
+							// Ambient occlusion actually has a bias since quads are triangles, and will work differently
+							// at different diagonals for every plane.
 							unsigned int aoLevel = 0; //must be 0-2 since light is 2-7
 							int3 aoOffset = ((vertLocalPos * 2) - 1);
 							int3 aoCheckPos1 = checkPos + aoDirections[side][i][0];
 							int3 aoCheckPos2 = checkPos + aoDirections[side][i][1];
 							int3 aoCheckPos3 = checkPos + aoDirections[side][i][2];
 
-							//check #1 last since its the corner block, which has the weakest shadow
-							if      (GetBlockFreeform(mem, aoCheckPos3, chunk) != Block::AIR)
+							// Check aoCheckPos1 last since its the corner block, which has the weakest shadow
+							// Having an effect of 1 for every case creates shadows of equal intensity for edges and corners
+							if (GetBlockFreeform(mem, aoCheckPos3, chunk) != Block::AIR)
 								aoLevel = 1;
 							else if (GetBlockFreeform(mem, aoCheckPos2, chunk) != Block::AIR)
 								aoLevel = 1;
 							else if (GetBlockFreeform(mem, aoCheckPos1, chunk) != Block::AIR)
 								aoLevel = 1; //could be 1 for lighter corners
 
+							// Determine block values that will need to be packed into this vertex
 							int uvID = i;
 							int blockID = (int)(blockType); // must be less than 1024
 							int lightValue = sideLightValues[side] - aoLevel; //must be between 0-7. AO is 0-2
+
+							// Bitpack all the information calculated so far. This will be parsed by the bitwise shader.
+							// Packing this information reduces memory footprint from 32 * 7 = 224 bits down to 32 bits.
+							// That is a video memory reduction of SEVEN times!
+							//each position is 0-31   (32)   so we use 15 bits, 5 for each pos
+							//the uv        is 0-3    (4)    so we use 2 bits
+							//the blockID   is 0-1023 (1024) so we use 10 bits
+							//the lightVal  is 0-7    (8)    so we use 3 bits
+							//the total bitcount is 30	 bits
 							GLuint compressedVertex = vertObjectPos.x | vertObjectPos.y << 5 | vertObjectPos.z << 10 | uvID << 15 | blockID << 17 | lightValue << 27;
 							mesh.PushVertex(compressedVertex);
-							
-							//debug, print out binary data of any vertices of block 0, 0, 0
+
+							// Debugging tool: print out binary data of any vertices of block 0, 0, 0
 							//if (blockPos.x == 0 && blockPos.y == 0 && blockPos.z == 0) {
 							//	std::cout << std::bitset<32>(compressedVertex) << std::endl;
 						}
 
+						// Push indices in an order that designates 2 clockwise triangles, composing a quad with 4 vertices
 						unsigned int indexStart = (mesh.index_count / 6) * 4;
 						mesh.PushIndices({
 							indexStart,
